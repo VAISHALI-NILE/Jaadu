849a992a3357c04e34fd3aee269c251978d53f33
Rudra <rudrakshnile930@gmail.com>
2023-11-19 18:10:17 +0530

    voice command startup

--- app/src/main/java/com/example/jaadu/MainActivity.java
+++ app/src/main/java/com/example/jaadu/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jaadu;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -56,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
--- app/src/main/java/com/example/jaadu/WelcomeActivity.java
+++ app/src/main/java/com/example/jaadu/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.example.jaadu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,13 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        System.out.println("In Welcome");
+        ScreenReceiver screenReceiver = new ScreenReceiver(); // Initialize the ScreenReceiver
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_USER_PRESENT); // Use ACTION_USER_PRESENT
+        registerReceiver(screenReceiver, filter);
+
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
